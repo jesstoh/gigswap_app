bdc3b39ad3ab21f330140131ca298be0f90c9865
jesstoh <jesstoh23@gmail.com>
2021-05-24 01:19:55 +0800

    Create header for not authenticated users

--- src/components/headers/AdminHeader.jsx
+++ src/components/headers/AdminHeader.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Navbar, Nav, NavDropdown} from 'react-bootstrap';
-import { FaUser, FaBell, FaComment } from "react-icons/fa"
+import { FaUser} from "react-icons/fa"
 
 function AdminHeader() {
   return (
