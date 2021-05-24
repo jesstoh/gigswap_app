9e89b2924c81dcbbecdf875a97b613e9b913f6c3
jesstoh <jesstoh23@gmail.com>
2021-05-24 22:23:43 +0800

    Created login thunk

--- src/slices/authenticationSlice.js
+++ src/slices/authenticationSlice.js
@@ -17,10 +17,20 @@ export const checkAuth = createAsyncThunk(
       `${process.env.REACT_APP_API_URL}/api/auth/`,
       { headers: { authorization: `Bearer ${accessToken}` } }
     );
-    console.log(response.data);
+    // console.log(response.data);
     return response.data;
   }
 );
+
+export const login = createAsyncThunk('authentication/login', async (data) => {
+  const accessToken = localStorage.getItem('access');
+  const response = await axios.post(
+    `${process.env.REACT_APP_API_URL}/api/login/`,
+    data,
+    { headers: { authorization: `Bearer ${accessToken}` } }
+  );
+  return response.data;
+});
 
 const authenticationSlice = createSlice({
   name: 'authentication',
@@ -38,6 +48,14 @@ const authenticationSlice = createSlice({
     builder.addCase(checkAuth.fulfilled, (state, action) => {
       console.log(action.payload);
       return { ...action.payload, status: 'success' };
+    });
+    builder.addCase(login.fulfilled, (state, action) => {
+      const { user } = action.payload;
+      state.isAuthenticated = true;
+      state.isHirer = user.is_hirer;
+      state.isAdmin = user.is_staff;
+      state.user.username = user.username;
+      state.status = 'fulfilled';
     });
   },
 });
