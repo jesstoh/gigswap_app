e5f8cfb32493c4fbce17315ae43cc08e4d0db8d2
jesstoh <jesstoh23@gmail.com>
2021-05-25 23:08:23 +0800

    Change login form implemetation to formik

--- src/pages/authentication/Login.jsx
+++ src/pages/authentication/Login.jsx
@@ -1,33 +1,51 @@
 import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { unwrapResult } from '@reduxjs/toolkit';
+import * as yup from 'yup';
+import { useFormik } from 'formik';
 import { Container, Row, Col, Form, Button } from 'react-bootstrap';
 import { login } from '../../slices/authenticationSlice';
 
 function Login() {
   const dispatch = useDispatch();
 
-  const [formValue, setFormValue] = useState({
-    username: '',
-    password: '',
+  const schema = yup.object().shape({
+    username: yup.string().required(),
+    password: yup.string().required(),
   });
-  // const [error, setError] = useState({})
 
-  function handleChange(e) {
-    setFormValue({ ...formValue, [e.target.id]: e.target.value });
-  }
-
-  async function handleSubmit(e) {
-    e.preventDefault();
-    try {
-      const result = await dispatch(login(formValue));
-      unwrapResult(result);
-      
-    } catch (err) {
-      setFormValue({ username: '', password: '' });
-      console.log(err);
+  const formik = useFormik({
+    initialValues: {
+      username: '',
+      password: '',
+      isSubmitted: false,
+    },
+    validationSchema: schema,
+    onSubmit: (values) => {
+      dispatch(login(values));
     }
-  }
+  })
+
+  // const [formValue, setFormValue] = useState({
+  //   username: '',
+  //   password: '',
+  // });
+  // // const [error, setError] = useState({})
+
+  // function handleChange(e) {
+  //   setFormValue({ ...formValue, [e.target.id]: e.target.value });
+  // }
+
+  // async function handleSubmit(e) {
+  //   e.preventDefault();
+  //   try {
+  //     const result = await dispatch(login(formValue));
+  //     unwrapResult(result);
+  //   } catch (err) {
+  //     setFormValue({ username: '', password: '' });
+  //     console.log(err);
+  //   }
+  // }
 
   return (
     <Container>
@@ -37,16 +55,19 @@ function Login() {
           className="py-4 px-4 shadow-sm p-3 mb-5 bg-white rounded-lg border"
         >
           <h3 className="text-center">Login</h3>
-          <Form>
+          <Form onSubmit={(e) => {
+                e.preventDefault();
+                formik.handleSubmit();
+              }}>
             <Form.Group>
               <Form.Label>Username</Form.Label>
               <Form.Control
                 required={true}
-                type="text"
-                placeholder="Enter email"
                 id="username"
-                value={formValue.username}
-                onChange={handleChange}
+                name="username"
+                value={formik.values.username}
+                onChange={formik.handleChange}
+                minLength={6}
               />
             </Form.Group>
             <Form.Group>
@@ -54,17 +75,18 @@ function Login() {
               <Form.Control
                 required
                 type="password"
-                placeholder="Password"
                 id="password"
-                value={formValue.password}
-                onChange={handleChange}
+                name="password"
+                value={formik.values.password}
+                onChange={formik.handleChange}
+                minLength={6}
               />
             </Form.Group>
             <Form.Text className="text-muted">
               Not a member yet? <a href="/register">Sign Up now</a>
             </Form.Text>
             <div className="text-center mt-3">
-              <Button variant="primary" type="submit" onClick={handleSubmit}>
+              <Button variant="primary" type="submit" >
                 Submit
               </Button>
             </div>
