f4ebc004049211e3b2bdb1271a62ce6ae81ac138
jesstoh <jesstoh23@gmail.com>
2021-05-26 22:16:35 +0800

    Show spinner & error message if fetchGig pending & rejected respectively

--- src/components/gigs/GigsList.jsx
+++ src/components/gigs/GigsList.jsx
@@ -1,20 +1,35 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import GigExcerpt from './GigExcerpt';
-import { Container } from 'react-bootstrap';
+import { Container, Spinner } from 'react-bootstrap';
 
 function GigsList() {
-  const gigs = useSelector((state) => state.gigs.gigs);
+  // const gigs = useSelector((state) => state.gigs.gigs);
+  const { gigs, status, error } = useSelector((state) => state.gigs);
+  let content;
 
-  return !gigs.length ? (
-    <span>No relevant gig listing</span>
-  ) : (
-    <Container>
-      {gigs.map((gig) => (
-        <GigExcerpt key={gig.id} gig={gig} />
-      ))}
-    </Container>
-  );
+  // If status is loading, show spinner
+  if (status === 'loading') {
+    content = <div className='text-center mt-5'><Spinner animation="border" variant='primary'/></div>;
+  } else if (status === 'succeeded') {
+    if (!gigs.length) {
+      //Show message if there is empty gig list
+      content = <span>No relevant gig listing</span>;
+    } else {
+      content = (
+        <Container>
+          {gigs.map((gig) => (
+            <GigExcerpt key={gig.id} gig={gig} />
+          ))}
+        </Container>
+      );
+    }
+  } else if (status === 'failed') {
+    //Show error if fetch failed
+    content = <span>{error}</span>;
+  }
+
+  return <React.Fragment>{content}</React.Fragment>;
 }
 
 export default GigsList;
