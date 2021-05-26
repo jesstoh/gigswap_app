a22dccb2a83f04351181948b7e677048ddb6b35c
jesstoh <jesstoh23@gmail.com>
2021-05-26 14:07:39 +0800

    Fix bug of response interceptor going into loop  when refresh token exists but invalid

--- src/utilz/Axios.js
+++ src/utilz/Axios.js
@@ -5,6 +5,8 @@ Axios.interceptors.request.use(
   (config) => {
     // Add access token to Authorization header before sending request
     config.headers.Authorization = `Bearer ${localStorage.getItem('access')}`;
+    // config.retry = false;
+    // console.log(config._retry)
     return config;
   },
   (error) => {
@@ -20,28 +22,37 @@ Axios.interceptors.response.use(
     const originalRequest = error.config;
     const refreshToken = localStorage.getItem('refresh');
     // If access token expired or not valid, and there is refresh toke in local storage, send request to get new access token
+
     // console.log(originalRequest.get('_retry'))
-    // console.log(originalRequest['_retry'])
-    console.log(originalRequest)
+    // console.log(originalRequest['_retry']
+    // console.log(error.config)
+    // console.log(error.config._retry)
+    // console.log(error.config.headers)
+    // console.log(error.config.url)
+
     if (
       refreshToken &&
       error.response.status === 401 &&
       !originalRequest._retry
     ) {
-    //   originalRequest._retry = false;
       originalRequest._retry = true;
 
-      console.log(originalRequest)
+    //   console.log(originalRequest)
+
+    //   console.log(originalRequest._retry)
 
       return Axios.post(`${process.env.REACT_APP_API_URL}/api/token/refresh/`, {
         refresh: refreshToken,
-      }).then((res) => {
+      }, {_retry: true}).then((res) => {
         if (res.status === 200) {
           // If refresh token is valid, store access & refresh tokens sent back from api in local storage
           localStorage.setItem('access', res.data.access);
           localStorage.setItem('refresh', res.data.refresh);
-          console.log('retrying')
-          // Resend original request
+
+        //   console.log('retrying')
+        //   console.log(originalRequest)
+
+            // Resend original request
           return Axios(originalRequest);
         }
       });
