8e9eb1a70c1b19b94150e818e5f1b38ed2804aa1
jesstoh <jesstoh23@gmail.com>
2021-05-26 18:50:28 +0800

    Dispatch fetchGig upon component mount for Gigs page

--- src/pages/gigs/Gigs.jsx
+++ src/pages/gigs/Gigs.jsx
@@ -1,10 +1,20 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { Container } from 'react-bootstrap';
+import GigsList from '../../components/gigs/GigsList';
+import { fetchGigs } from '../../slices/gigsSlice.js';
 
 function Gigs() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchGigs());
+  }, [dispatch]);
+
   return (
-    <div>
-      <h1>All Gigs Page</h1>
-    </div>
+    <Container>
+      <GigsList />
+    </Container>
   );
 }
 
