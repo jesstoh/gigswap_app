62f577de724eec54d982b08b4f2babe698ee189a
jesstoh <jesstoh23@gmail.com>
2021-05-26 19:07:09 +0800

    Design - Change style of GigExcerpt component

--- src/components/gigs/GigExcerpt.jsx
+++ src/components/gigs/GigExcerpt.jsx
@@ -3,22 +3,23 @@ import { Card, Badge } from 'react-bootstrap';
 
 function GigExcerpt({ gig }) {
   return (
-    <Card>
+    <Card className='mb-1 gig-card shadow-sm p-1 bg-white rounded'>
       <a href={`/gigs/${gig.id}`}>
         <Card.Body>
           <Card.Title>{gig.title}</Card.Title>
+          <Card.Subtitle className="mb-2 text-muted">
+            by {gig.poster.username}
+          </Card.Subtitle>
           <div>
             {gig.subcategories.map((subcat, index) => {
               return (
-                <Badge pill variant="info" key={index}>
+                <Badge pill variant="info" key={index} className='mr-2'>
                   {subcat.name}
                 </Badge>
               );
             })}
           </div>
-          <Card.Subtitle className="mb-2 text-muted">
-            by {gig.poster.username}
-          </Card.Subtitle>
+
           <Card.Text>{gig.description}</Card.Text>
           <span>Posted on {gig.created_at}</span>
           <span>Expired on {gig.expired_at} </span>
