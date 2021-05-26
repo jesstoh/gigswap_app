8c12afd9e30715e61a30a50761a4cb934de94a87
jesstoh <jesstoh23@gmail.com>
2021-05-26 18:56:29 +0800

    Design - Gigs component change layout

--- src/pages/gigs/Gigs.jsx
+++ src/pages/gigs/Gigs.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
-import { Container } from 'react-bootstrap';
+import { Container, Row, Col } from 'react-bootstrap';
 import GigsList from '../../components/gigs/GigsList';
 import { fetchGigs } from '../../slices/gigsSlice.js';
 
@@ -9,11 +9,17 @@ function Gigs() {
 
   useEffect(() => {
     dispatch(fetchGigs());
-  }, [dispatch]);
+  }, []);
 
   return (
-    <Container>
-      <GigsList />
+    <Container className="pt-3">
+      <Row className='border my-3'>Placeholder for search </Row>
+      <Row>
+        <Col md={3} className='border'>Placeholder for filter </Col>
+        <Col md={9}>
+          <GigsList />
+        </Col>
+      </Row>
     </Container>
   );
 }
