ef91b2beb186650085486ad0519593cd3a9684e5
jesstoh <jesstoh23@gmail.com>
2021-05-26 17:51:55 +0800

    Reducer - create async thunk for fetch gigs

--- src/slices/gigsSlice.js
+++ src/slices/gigsSlice.js
@@ -1,4 +1,5 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import Axios from '../utilz/Axios';
 
 const initialState = {
   gigs: [],
@@ -6,13 +7,31 @@ const initialState = {
   error: null,
 };
 
+export const fetchGigs = createAsyncThunk('gigs/fetchGigs', async () => {
+  const gigs = await Axios.get(`${process.env.REACT_APP_API_URL}/api/gigs/`);
+});
+
 const gigsSlice = createSlice({
-    name: 'gigs',
-    initialState,
-    reducers: {
-
-    }
-})
-
+  name: 'gigs',
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    // Update gigs state when fetchGigs success
+    builder.addCase(fetchGigs.fulfilled, (state, action) => {
+      state.gigs = action.payload;
+      state.status = 'succeeded';
+    });
+    // Change status to loading when fetching
+    builder.addCase(fetchGigs.pending, (state, action) => {
+      state.status = 'loading';
+    });
+    // Change status to failed & set error, when fetchGigs rejected
+    builder.addCase(fetchGigs.rejected, (state, action) => {
+      state.status = 'failed';
+      // Change it to other content later
+      state.error = action.error.message;
+    });
+  },
+});
 
 export default gigsSlice.reducer;
