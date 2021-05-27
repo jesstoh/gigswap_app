a69a970657a5e5e29cfca312debb4377d87a9b3a
jesstoh <jesstoh23@gmail.com>
2021-05-27 16:40:22 +0800

    Create fetchProfile thunk

--- src/slices/profileSlice.js
+++ src/slices/profileSlice.js
@@ -1,18 +1,46 @@
-import {createSlice, createAsyncThunk} from '@reduxjs/toolkit'
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import { act } from 'react-dom/test-utils';
+import Axios from '../utilz/Axios';
 
 const initialState = {
-    profile: null,
-    status: 'idle',
-    error: null
-}
+  profile: null,
+  status: 'idle',
+  error: null,
+};
+
+export const fetchProfile = createAsyncThunk(
+  'profile/fetchProfile',
+  async (_, { rejectWithValue }) => {
+    try {
+      const response = await Axios.get(
+        `${process.env.REACT_APP_API_URL}/api/profile/`
+      );
+      return response.data;
+    } catch (err) {
+      const { data, status } = err.response;
+      return rejectWithValue({ data, status });
+    }
+  }
+);
 
 // Create slice for keeping profile of login in talent & hirer
 const profileSlice = createSlice({
-    name:'profile',
-    initialState,
-    reducers: {}
-})
-
-
+  name: 'profile',
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+      builder.addCase(fetchProfile.fulfilled, (state, action) => {
+          state.status = 'succeeded'
+          state.profile = action.payload
+      });
+      builder.addCase(fetchProfile.pending, (state, action) => {
+          state.status = 'loading'
+      });
+      builder.addCase(fetchProfile.rejected, (state, action) => {
+          state.status = 'failed'
+          state.error = action.payload.data.detail
+      })
+  }
+});
 
 export default profileSlice.reducer;
