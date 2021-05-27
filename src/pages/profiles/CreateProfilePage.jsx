0d615aa3f027985ca217628d8272056f2c8bfc02
jesstoh <jesstoh23@gmail.com>
2021-05-27 20:35:04 +0800

    Redirect user to profile page from create profile form if profile is completed

--- src/pages/profiles/CreateProfilePage.jsx
+++ src/pages/profiles/CreateProfilePage.jsx
@@ -1,8 +1,24 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import { Redirect } from 'react-router-dom';
 import { Container } from 'react-bootstrap';
 
 function CreateProfilePage() {
-  return <Container>Create Profile Form</Container>;
+  // Get current profile complete status of user
+  const isProfileComplete = useSelector(
+    (state) => state.authentication.isProfileComplete
+  );
+
+  let content;
+
+  // Redirect to profile page if profile is completed
+  if (isProfileComplete) {
+    content = <Redirect to="/profile" />;
+  } else {
+    content = <div>Create Profile Form</div>;
+  }
+
+  return <Container>{content}</Container>;
 }
 
 export default CreateProfilePage;
