e32039bd68ed960baf48755506fe93eb4892e933
jesstoh <jesstoh23@gmail.com>
2021-05-27 18:04:41 +0800

    Create HirerProfileDetails component

--- src/components/profiles/HirerProfileDetails.jsx
+++ src/components/profiles/HirerProfileDetails.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
-import { Container, Row, Col, Badge } from 'react-bootstrap';
+import { Container, Row, Col} from 'react-bootstrap';
 
 function HirerProfileDetails() {
   const profile = useSelector((state) => state.profile.profile);
   return (
-    <Container>
-      <Row>
+    <Container className='mt-5'>
+      <Row className='mb-4'>
         <Col xs="4">
           <img src={profile.image} className="img-fluid img-thumbnail " />
         </Col>
@@ -30,43 +30,12 @@ function HirerProfileDetails() {
           <Col>{profile.user.last_name}</Col>
         </Row>
       </div>
-      <div className="bio mb-4">
-        <h6>Bio</h6>
+      <div className="mb-4">
+        <h6>Company</h6>
         <Row>
-          <Col sm="4">Description: </Col>
-          <Col>{profile.bio}</Col>
+          <Col sm="4">Company Name: </Col>
+          <Col>{profile.company}</Col>
         </Row>
-        <Row>
-          <Col sm="4">Skills: </Col>
-          <Col>
-            {' '}
-            {profile.skills.map((subcat, index) => {
-              return (
-                <Badge pill variant="info" key={index} className="mr-2">
-                  {subcat.name}
-                </Badge>
-              );
-            })}
-          </Col>
-        </Row>
-      </div>
-      <div className='preference mb-4'>
-        <h6>Preference</h6>
-        <Row>
-          <Col sm="4">Remote Only: </Col>
-          <Col>{profile.remote ? 'Yes' : 'No'}</Col>
-        </Row>
-        <Row>
-          <Col sm="4">Project or Fixed Term: </Col>
-          <Col>{profile.fixed_term ? 'Fixed term' : 'Project based'}</Col>
-        </Row>
-        <Row>
-          <Col sm="4">Min pay: </Col>
-          <Col>$ {profile.min_pay} /hour</Col>
-        </Row>
-      </div>
-      <div className='other-particular'>
-        <h6>Other</h6>
         <Row>
           <Col sm="4">Address: </Col>
           <Col>{profile.address}</Col>
