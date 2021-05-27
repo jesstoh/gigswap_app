cc955cb7d4f17cfe5de6f39368a43f2f4d770897
jesstoh <jesstoh23@gmail.com>
2021-05-27 17:07:50 +0800

    Rendering profile content upon status of fetching

--- src/pages/profiles/ProfilePage.jsx
+++ src/pages/profiles/ProfilePage.jsx
@@ -1,21 +1,42 @@
 import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { Container, Row, Col } from 'react-bootstrap';
-import {fetchProfile} from '../../slices/profileSlice.js';
+import { Container, Row, Col, Spinner } from 'react-bootstrap';
+import { fetchProfile } from '../../slices/profileSlice.js';
 
 function ProfilePage() {
   const dispatch = useDispatch();
   const profile = useSelector((state) => state.profile.profile);
+  const status = useSelector((state) => state.profile.status);
+  const error = useSelector((state) => state.profile.error);
 
   useEffect(() => {
     dispatch(fetchProfile());
+  }, []);
 
-  }, [dispatch])
+  let content;
 
-  return (<Container>
+  if (status === 'loading') {
+    content = (
+      <div className="text-center mt-5">
+        <Spinner animation="border" variant="primary" />
+      </div>
+    );
+  } else if (status === 'succeeded') {
+    content = (
+      <Container>
+        <Row>
+          <Col xs="4">
+            <img src={profile.image} className="img-fluid img-thumbnail " />
+          </Col>
+        </Row>
+      </Container>
+    );
+  } else if (status === 'failed') {
+    //Show error if fetch failed
+    content = <span>{error}</span>;
+  }
 
-
-  </Container>);
+  return <section>{content}</section>;
 }
 
 export default ProfilePage;
