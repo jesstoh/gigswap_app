a2eae9a14583a922b9f866a26d95d128bc2ddffd
jesstoh <jesstoh23@gmail.com>
2021-05-27 10:48:15 +0800

    Style - change card description truncated & other font alignment

--- src/components/gigs/GigExcerpt.jsx
+++ src/components/gigs/GigExcerpt.jsx
@@ -5,7 +5,7 @@ import TimeAgo from '../others/TimeAgo';
 
 function GigExcerpt({ gig }) {
   return (
-    <Card className='mb-1 gig-card shadow-sm p-1 bg-white rounded'>
+    <Card className="mb-1 gig-card shadow-sm  bg-white rounded">
       <a href={`/gigs/${gig.id}`}>
         <Card.Body>
           <Card.Title>{gig.title}</Card.Title>
@@ -15,17 +15,26 @@ function GigExcerpt({ gig }) {
           <div>
             {gig.subcategories.map((subcat, index) => {
               return (
-                <Badge pill variant="info" key={index} className='mr-2'>
+                <Badge pill variant="info" key={index} className="mr-2">
                   {subcat.name}
                 </Badge>
               );
             })}
           </div>
-
-          <Card.Text>{gig.description}</Card.Text>
-          <TimeAgo timestamp={gig.created_at} text='Posted '/><br/>
-          <span className='text-muted'><i>Expired on {format(parseISO(gig.expired_at),'d MMM yyyy' )} </i></span><span className='text-muted float-right'>{0 || gig.applicants.length} applied </span>
-        
+          <Card.Text className='text-truncate'>{gig.description}</Card.Text>
+          <div className="card-sm-text">
+            <TimeAgo timestamp={gig.created_at} text="Posted " />
+            <br />
+            <span className="text-muted">
+              <i>
+                Expired on {format(parseISO(gig.expired_at), 'd MMM yyyy')}{' '}
+              </i>
+            </span>
+            <span className="text-muted float-right">
+              {0 || gig.applicants.length} applied{' '}
+            </span>
+          </div>
+          
         </Card.Body>
       </a>
     </Card>
