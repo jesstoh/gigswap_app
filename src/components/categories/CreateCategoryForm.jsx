429d5c89aea7e6ab795f1cf525b765f4748841cf
jesstoh <jesstoh23@gmail.com>
2021-05-27 21:37:04 +0800

    Design - Create Create category form component

---
+++ src/components/categories/CreateCategoryForm.jsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import { useFormik } from 'formik';
+import * as yup from 'yup';
+import { Container, Row, Col, Form, Button } from 'react-bootstrap';
+
+function CreateCategoryForm() {
+  const schema = yup.object().shape({
+    name: yup.string().required(),
+  });
+
+  const formik = useFormik({
+    initialValues: {
+      name: '',
+      isSubmitted: false,
+    },
+    validationSchema: schema,
+    onSubmit: (values) => {},
+  });
+
+  return (
+    <Container>
+      <h3 className="text-center">Add New Category</h3>
+      <Form
+        onSubmit={(e) => {
+          e.preventDefault();
+          formik.handleSubmit();
+        }}
+      >
+        <Form.Group>
+          <Form.Label>Category Name</Form.Label>
+          <Form.Control
+            required={true}
+            id="name"
+            name="name"
+            value={formik.values.name}
+            onChange={formik.handleChange}
+          />
+        </Form.Group>
+
+        <div className="text-center mt-3">
+          <Button variant="primary" type="submit">
+            Submit
+          </Button>
+        </div>
+      </Form>
+    </Container>
+  );
+}
+
+export default CreateCategoryForm;
