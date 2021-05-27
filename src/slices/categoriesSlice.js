357d91f6ea96a679ab111d7553ea9da5dfc16341
jesstoh <jesstoh23@gmail.com>
2021-05-27 21:58:01 +0800

    Create createCategory reducer for adding new category by admin user

--- src/slices/categoriesSlice.js
+++ src/slices/categoriesSlice.js
@@ -21,6 +21,22 @@ export const fetchCategories = createAsyncThunk(
   }
 );
 
+export const addCategory = createAsyncThunk(
+  'categories/addCategory',
+  async (data, { rejectWithValue }) => {
+    try {
+      const response = await Axios.post(
+        `${process.env.REACT_APP_API_URL}/api/categories/create/`,
+        data
+      );
+      return response.data;
+    } catch (err) {
+      const { data, status } = err.response;
+      return rejectWithValue({ data, status });
+    }
+  }
+);
+
 export const fetchSubcats = createAsyncThunk(
   'categories/fetchSubcats',
   async (_, { rejectWithValue }) => {
@@ -63,6 +79,9 @@ const categoriesSlice = createSlice({
       state.subcats.status = 'failed';
       state.subcats.error = action.payload.data.detail;
     });
+    builder.addCase(addCategory.fulfilled, (state, action) => {
+        state.subcats.content.push(action.payload);
+      });
   },
 });
 
