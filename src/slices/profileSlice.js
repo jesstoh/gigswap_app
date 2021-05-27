1ed847b0bdc99d0c0cf3342e3fbdaacdebf1e83c
jesstoh <jesstoh23@gmail.com>
2021-05-27 19:44:52 +0800

    Create reducer to toggle state.edit boolean value

--- src/slices/profileSlice.js
+++ src/slices/profileSlice.js
@@ -6,7 +6,7 @@ const initialState = {
   profile: null,
   status: 'idle',
   error: null,
-  edit: false
+  edit: false,
 };
 
 export const fetchProfile = createAsyncThunk(
@@ -28,20 +28,26 @@ export const fetchProfile = createAsyncThunk(
 const profileSlice = createSlice({
   name: 'profile',
   initialState,
-  reducers: {},
+  reducers: {
+    toggleProfileEdit(state, action) {
+      state.edit = !state.edit;
+    },
+  },
   extraReducers: (builder) => {
-      builder.addCase(fetchProfile.fulfilled, (state, action) => {
-          state.status = 'succeeded'
-          state.profile = action.payload
-      });
-      builder.addCase(fetchProfile.pending, (state, action) => {
-          state.status = 'loading'
-      });
-      builder.addCase(fetchProfile.rejected, (state, action) => {
-          state.status = 'failed'
-          state.error = action.payload.data.detail
-      })
-  }
+    builder.addCase(fetchProfile.fulfilled, (state, action) => {
+      state.status = 'succeeded';
+      state.profile = action.payload;
+    });
+    builder.addCase(fetchProfile.pending, (state, action) => {
+      state.status = 'loading';
+    });
+    builder.addCase(fetchProfile.rejected, (state, action) => {
+      state.status = 'failed';
+      state.error = action.payload.data.detail;
+    });
+  },
 });
 
 export default profileSlice.reducer;
+
+export const { toggleProfileEdit } = profileSlice.actions;
