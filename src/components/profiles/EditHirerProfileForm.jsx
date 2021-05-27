e2b74b323af1f63ef2e3d0cfcddff54dabb9f4f8
jesstoh <jesstoh23@gmail.com>
2021-05-27 19:33:09 +0800

    Create skeletons of Edit Hire Profile and Talent forms

---
+++ src/components/profiles/EditHirerProfileForm.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import {Container} from 'react-bootstrap'
+
+function EditHireProfileForm(){
+
+
+    return (<Container>Edit Hire Profile Form</Container>)
+
+}
+
+
+export default EditHireProfileForm;
