070e5e53f3c16c6eb808d4d5623d53ff2cda96ee
jesstoh <jesstoh23@gmail.com>
2021-05-28 12:07:04 +0800

    Create CategoriesList component to display all the categories in a table

--- src/pages/admin/CategoriesPage.jsx
+++ src/pages/admin/CategoriesPage.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
 import CreateCategoryForm from '../../components/categories/CreateCategoryForm';
+import CategoriesList from '../../components/categories/CategoriesList'
 
 function CategoriesPage() {
   return (
@@ -8,7 +9,7 @@ function CategoriesPage() {
       <Row>
         <Col md="5">
           <CreateCategoryForm />
-          <div>Category List</div>
+          <CategoriesList/>
         </Col>
         <Col>
           <div>Create Subcategry</div>
