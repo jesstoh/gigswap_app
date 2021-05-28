da1b007746037a5aea85b11d23ccaa0148905d57
jesstoh <jesstoh23@gmail.com>
2021-05-28 15:21:10 +0800

    Create fetch notifications reducer

--- src/slices/notificationsSlice.js
+++ src/slices/notificationsSlice.js
@@ -1,4 +1,5 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import Axios from '../utilz/Axios';
 
 const initialState = {
   notifications: [],
@@ -7,10 +8,37 @@ const initialState = {
   error: null,
 };
 
+export const fetchNotifications = createAsyncThunk(
+  'notifications/fetchNotifications',
+  async (_, { rejectWithValue }) => {
+    try {
+      const response = await Axios.get(
+        `${process.env.REACT_APP_API_URL}/api/notifications/`
+      );
+      return response.data;
+    } catch (error) {
+      const { data, status } = error.response;
+      return rejectWithValue({ data, status });
+    }
+  }
+);
 const notificationsSlice = createSlice({
   name: 'notifications',
   initialState,
   reducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(fetchNotifications.fulfilled, (state, action) => {
+      state.notifications = action.payload;
+      state.status = 'succeeded';
+    });
+    builder.addCase(fetchNotifications.pending, (state, action) => {
+      state.status = 'loading';
+    });
+    builder.addCase(fetchNotifications.rejected, (state, action) => {
+      state.status = 'failed';
+      state.error = action.payload.data;
+    });
+  },
 });
 
 export default notificationsSlice.reducer;
