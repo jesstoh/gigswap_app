942b31f50bd5df63ce11f587f002d144f4a0752f
jesstoh <jesstoh23@gmail.com>
2021-05-28 11:33:52 +0800

    Render create category form based on edit state

--- src/components/categories/CreateCategoryForm.jsx
+++ src/components/categories/CreateCategoryForm.jsx
@@ -9,35 +9,80 @@ import { addCategory } from '../../slices/categoriesSlice.js';
 function CreateCategoryForm() {
   const dispatch = useDispatch();
   const [errorMessage, setErrorMessage] = useState(null);
+  const [name, setName] = useState('');
+  const [edit, setEdit] = useState(false);
 
-  const schema = yup.object().shape({
-    name: yup.string().required(),
-  });
+  //   const schema = yup.object().shape({
+  //     name: yup.string().required(),
+  //   });
 
-  async function handleSubmit(data) {
+  async function handleSubmit(e) {
+    e.preventDefault();
+    const formatName = name[0].toUpperCase() + name.slice(1).toLowerCase();
+    const data = { name: formatName };
     try {
       const result = await dispatch(addCategory(data));
       unwrapResult(result);
     } catch (err) {
       console.log(err);
       setErrorMessage(Object.values(err.data)[0][0]);
+    } finally{
+        setName('')
+        setEdit(false)
     }
+
   }
 
-  const formik = useFormik({
-    initialValues: {
-      name: '',
-      isSubmitted: false,
-    },
-    validationSchema: schema,
-    onSubmit: (values) => {
-      console.log(values);
-      handleSubmit({ name: values.name });
-      values.name = '';
-      values.isSubmitted = true;
-      console.log(values);
-    },
-  });
+  //   const formik = useFormik({
+  //     initialValues: {
+  //       name: '',
+  //       isSubmitted: true,
+  //     },
+  //     validationSchema: schema,
+  //     onSubmit: (values) => {
+  //       console.log(values);
+  //       handleSubmit({ name: values.name });
+  //       values.name = '';
+  //       values.isSubmitted = true;
+  //       console.log(values);
+  //     },
+  //   });
+
+  let content;
+
+  if (!edit) {
+    content = (
+      <Button
+        onClick={() => {
+          setEdit(true);
+          setErrorMessage(null);
+        }}
+      >
+        Add category
+      </Button>
+    );
+  } else {
+    content = (
+      <Form onSubmit={handleSubmit}>
+        <Form.Group>
+          <Form.Label>Category Name</Form.Label>
+          <Form.Control
+            required={true}
+            id="name"
+            name="name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+        </Form.Group>
+
+        <div className="text-center mt-3">
+          <Button variant="primary" type="submit">
+            Add
+          </Button>
+        </div>
+      </Form>
+    );
+  }
 
   return (
     <Container>
@@ -50,30 +95,7 @@ function CreateCategoryForm() {
           {errorMessage}
         </Alert>
       )}
-
-      <Form
-        onSubmit={(e) => {
-          e.preventDefault();
-          formik.handleSubmit();
-        }}
-      >
-        <Form.Group>
-          <Form.Label>Category Name</Form.Label>
-          <Form.Control
-            required={true}
-            id="name"
-            name="name"
-            value={formik.values.name}
-            onChange={formik.handleChange}
-          />
-        </Form.Group>
-
-        <div className="text-center mt-3">
-          <Button variant="primary" type="submit">
-            Add
-          </Button>
-        </div>
-      </Form>
+      {content}
     </Container>
   );
 }
