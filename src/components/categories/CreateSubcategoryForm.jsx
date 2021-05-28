d683e757ea9e46cf886f66438a6e7df491065643
jesstoh <jesstoh23@gmail.com>
2021-05-28 12:52:58 +0800

    Able to add subcategory by dispatch addSubcategory

--- src/components/categories/CreateSubcategoryForm.jsx
+++ src/components/categories/CreateSubcategoryForm.jsx
@@ -1,21 +1,63 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React, { useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { unwrapResult } from '@reduxjs/toolkit';
 import { Form, Button } from 'react-bootstrap';
+import { capitalizeWord } from '../../utilz/format';
+import { addSubcategory } from '../../slices/categoriesSlice.js';
 
 function CreateSubcategoryForm() {
   const categories = useSelector(
     (state) => state.categories.cats.content
   ).slice();
 
+  const dispatch = useDispatch();
+  const [errorMessage, setErrorMessage] = useState(null);
+  const [formValue, setFormValue] = useState({ name: '', category: '' });
+  const [edit, setEdit] = useState(false);
+
+  function handleChange(e) {
+    setFormValue({ ...formValue, [e.target.name]: e.target.value });
+  }
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    const formatName = capitalizeWord(formValue.name);
+    const data = { name: formatName, category: formValue.category };
+    console.log(data)
+    try {
+      const result = await dispatch(addSubcategory(data));
+      unwrapResult(result);
+    } catch (err) {
+      console.log(err);
+      setErrorMessage(err.data.detail);
+    } finally {
+      setFormValue({ name: '', category: '' })
+    }
+    console.log(formValue);
+  }
+
   return (
-    <Form>
+    <Form onSubmit={handleSubmit}>
       <Form.Group>
         <Form.Label>Subcategory Name</Form.Label>
-        <Form.Control type="text" required name="name" />
+        <Form.Control
+          type="text"
+          required
+          name="name"
+          value={formValue.name}
+          onChange={handleChange}
+        />
       </Form.Group>
       <Form.Group>
         <Form.Label>Category</Form.Label>
-        <Form.Control as="select" name="category">
+        <Form.Control
+          as="select"
+          name="category"
+          value={formValue.category}
+          required
+          onChange={handleChange}
+        >
+          <option value="">Please select a category</option>
           {categories.map((cat) => (
             <option key={cat.id} value={cat.id}>
               {cat.name}
