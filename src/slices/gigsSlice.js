7b0e49fbd96ebcb524e0f0a1a34999f9c138c6ae
jesstoh <jesstoh23@gmail.com>
2021-05-29 00:44:47 +0800

    Create fetchRecommendedGigs reducer

--- src/slices/gigsSlice.js
+++ src/slices/gigsSlice.js
@@ -5,15 +5,34 @@ const initialState = {
   gigs: [],
   status: 'idle',
   error: null,
+  errorCode: null,
   activeGig: { gig: null, status: 'idle', error: null, edit: false },
 };
 
-export const fetchGigs = createAsyncThunk('gigs/fetchGigs', async () => {
-  const response = await Axios.get(
-    `${process.env.REACT_APP_API_URL}/api/gigs/`
-  );
-  return response.data;
-});
+export const fetchGigs = createAsyncThunk(
+  'gigs/fetchGigs',
+  async (_, { rejectWithValue }) => {
+    try {
+      const response = await Axios.get(
+        `${process.env.REACT_APP_API_URL}/api/gigs/`
+      );
+      return response.data;
+    } catch (err) {
+      const { data, status } = err.response;
+      return rejectWithValue({data, status})
+    }
+  }
+);
+
+export const fetchRecommendedGigs = createAsyncThunk(
+  'gigs/fetchRecommendedGigs',
+  async () => {
+    const response = await Axios.get(
+      `${process.env.REACT_APP_API_URL}/api/gigs/recommended/`
+    );
+    return response.data;
+  }
+);
 
 export const fetchSingleGig = createAsyncThunk(
   'gigs/fetchSingleGig',
@@ -25,7 +44,7 @@ export const fetchSingleGig = createAsyncThunk(
       return response.data;
     } catch (err) {
       const { data, status } = err.response;
-      console.log(err.response)
+      console.log(err.response);
       return rejectWithValue({ data, status });
     }
   }
@@ -53,16 +72,31 @@ const gigsSlice = createSlice({
       state.error = action.error.message;
     });
     builder.addCase(fetchSingleGig.pending, (state, action) => {
-        state.activeGig.status = 'loading'
+      state.activeGig.status = 'loading';
     });
-    builder.addCase(fetchSingleGig.fulfilled, (state,action) => {
-        state.activeGig.gig = action.payload
-        state.activeGig.status = 'succeeded'
-    })
+    builder.addCase(fetchSingleGig.fulfilled, (state, action) => {
+      state.activeGig.gig = action.payload;
+      state.activeGig.status = 'succeeded';
+    });
     builder.addCase(fetchSingleGig.rejected, (state, action) => {
-        state.activeGig.error = action.payload.data.detail
-        state.activeGig.status = 'failed'
-    })
+      state.activeGig.error = action.payload.data.detail;
+      state.activeGig.status = 'failed';
+    });
+    // Update gigs state when fetchGigs success
+    builder.addCase(fetchRecommendedGigs.fulfilled, (state, action) => {
+      state.gigs = action.payload;
+      state.status = 'succeeded';
+    });
+    builder.addCase(fetchRecommendedGigs.pending, (state, action) => {
+      state.status = 'loading';
+    });
+    // Change status to failed & set error, when fetchGigs rejected
+    builder.addCase(fetchRecommendedGigs.rejected, (state, action) => {
+      state.status = 'failed';
+      // Change it to other content later
+      state.error = action.payload.data.detail
+      state.errorCode = action.payload.status
+    });
   },
 });
 
