25cf50b59b717efcf7a004c1391745f00107f816
jesstoh <jesstoh23@gmail.com>
2021-05-28 16:39:47 +0800

    Able to display notifications on NotificationPage

--- src/pages/notifications/NotificationsPage.jsx
+++ src/pages/notifications/NotificationsPage.jsx
@@ -1,11 +1,32 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import { Spinner, Container } from 'react-bootstrap';
+import NotificationExcerpt from '../../components/notifications/NotificationExcerpt';
 
 function NotificationsPage() {
-  return (
-    <div>
-      <h1>Notifications Page</h1>
-    </div>
+  const { notifications, status, error } = useSelector(
+    (state) => state.notifications
   );
+
+  let content;
+
+  if (status === 'loading') {
+    content = (
+      <div className="text-center mt-5">
+        <Spinner animation="border" variant="primary" />
+      </div>
+    );
+  } else if (status === 'succeeded') {
+    content = (
+      <div>
+        {notifications.map((notification) => (
+          <NotificationExcerpt key={notification.id} notification={notification} />
+        ))}
+      </div>
+    );
+  }
+
+  return <Container>{content}</Container>;
 }
 
 export default NotificationsPage;
