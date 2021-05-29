d388f33975c4ee622577db3959e1e0ca0b3aa8f1
jesstoh <jesstoh23@gmail.com>
2021-05-29 08:05:28 +0800

    Create fetchTalentFav thunk

--- src/slices/favouritesSlicer.js
+++ src/slices/favouritesSlicer.js
@@ -7,11 +7,38 @@ const initialState = {
   error: null,
 };
 
+export const fetchTalentFav = createAsyncThunk('favourites/fetchTalentFav', async(_, {rejectWithValue}) => {
+    try {
+        const response = await Axios.get(
+          `${process.env.REACT_APP_API_URL}/api/talents/fav/`
+        );
+        return response.data;
+      } catch (err) {
+        const { data, status } = err.response;
+        return rejectWithValue({ data, status });
+      }
+})
+
 const favouritesSlice = createSlice({
   name: 'favourites',
   initialState,
   reducers: {},
-  extraReducers: (builder) => {},
+  extraReducers: (builder) => {
+      // Update talent favourite list when fetch success
+    builder.addCase(fetchTalentFav.fulfilled, (state, action) => {
+        state.fav = action.payload;
+        state.status = 'succeeded';
+      });
+      // Change status to loading when fetching
+      builder.addCase(fetchTalentFav.pending, (state, action) => {
+        state.status = 'loading';
+      });
+      // Change status to failed & set error, when not found
+      builder.addCase(fetchTalentFav.rejected, (state, action) => {
+        state.status = 'failed';
+        state.error = action.payload.data.detail
+      });
+  },
 });
 
 export default favouritesSlice.reducer;
