d7368a54b1b4888d3298f3aa26033a5985dc4c5f
jesstoh <jesstoh23@gmail.com>
2021-05-29 18:41:07 +0800

    Create thunk for fetching all talents

--- src/slices/talentsSlice.js
+++ src/slices/talentsSlice.js
@@ -2,20 +2,47 @@ import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import Axios from '../utilz/Axios';
 
 const initialState = {
-    talents: [],
-    status: 'idle',
-    error: null,
-    activeTalent: { talent: null, status: 'idle', error: null },
-}
+  talents: [],
+  status: 'idle',
+  error: null,
+  activeTalent: { talent: null, status: 'idle', error: null },
+};
+
+export const fetchTalents = createAsyncThunk(
+  'talents/fetchTalents',
+  async (_, { rejectWithValue }) => {
+    try {
+      const response = await Axios.get(
+        `${process.env.REACT_APP_API_URL}/api/talents/`
+      );
+      return response.data;
+    } catch (err) {
+      const { data, status } = err.response;
+      return rejectWithValue({ data, status });
+    }
+  }
+);
 
 const talentsSlice = createSlice({
-    name: 'talents',
-    initialState,
-    reducers: {},
-    extraReducers: builder => {
-
-    }
-})
-
+  name: 'talents',
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    // Update talent favourite list when fetch for login talent success
+    builder.addCase(fetchTalents.fulfilled, (state, action) => {
+      state.talents = action.payload;
+      state.status = 'succeeded';
+    });
+    // Change status to loading when fetching
+    builder.addCase(fetchTalents.pending, (state, action) => {
+      state.status = 'loading';
+    });
+    // Change status to failed & set error, when not found
+    builder.addCase(fetchTalents.rejected, (state, action) => {
+      state.status = 'failed';
+      state.error = action.payload.data.detail;
+    });
+  },
+});
 
 export default talentsSlice.reducer;
