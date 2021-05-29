18813ef0ac9768fb7b0df5c06c0cad37286e8d9c
jesstoh <jesstoh23@gmail.com>
2021-05-29 08:25:07 +0800

    Fetch talent fav list upon first render of TalentFav page & condition rendering error / spinner

--- src/pages/talents/TalentFav.jsx
+++ src/pages/talents/TalentFav.jsx
@@ -1,11 +1,33 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { Container, Row, Col, Spinner } from 'react-bootstrap';
+import { fetchTalentFav } from '../../slices/favouritesSlicer.js';
 
 function TalentFav() {
-  return (
-    <div>
-      <h1>Talent Fav Page</h1>
-    </div>
-  );
+  const dispatch = useDispatch();
+  const { fav, status, error } = useSelector((state) => state.favourites);
+
+  useEffect(() => {
+    dispatch(fetchTalentFav());
+  }, []);
+
+  let content;
+
+  if (status === 'loading') {
+    content = (
+      <div className="text-center mt-5">
+        <Spinner animation="border" variant="primary" />
+      </div>
+    );
+  } else if (status === 'succeeded') {
+    content = <div>Favorite container </div>;
+  } else if (status === 'failed') {
+    //Show error
+    content = <span>{error}</span>;
+  }
+
+  return <Container className='mt-5'>{content}</Container>;
+
 }
 
 export default TalentFav;
