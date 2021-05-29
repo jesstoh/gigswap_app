bb0dbfcb00ddbd99adcf91748f53ef207f69448e
jesstoh <jesstoh23@gmail.com>
2021-05-29 16:40:17 +0800

    Create SmallTalentExcerpt to display brief info of talent in card

---
+++ src/components/talents/SmallTalentExcerpt.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Card, Badge } from 'react-bootstrap';
+
+function SmallTalentExcerpt({ talent }) {
+  return (
+    <Card className="talent-card shadow-sm  bg-white rounded">
+      <a href={`/talents/${talent.user.id}`}>
+        <Card.Body>
+          <Card.Title>{talent.username}</Card.Title>
+          <Card.Subtitle className="mb-2 text-muted">
+            Review: ..... <br />
+            Gigs Won: {talent.gigs_won}
+          </Card.Subtitle>
+          <div>
+            {talent.skills.map((skill, index) => {
+              return (
+                <Badge pill variant="info" key={index} className="mr-2">
+                  {skill.name}
+                </Badge>
+              );
+            })}
+          </div>
+          <Card.Text className="text-truncate">{talent.bio}</Card.Text>
+        </Card.Body>
+      </a>
+    </Card>
+  );
+}
+
+export default SmallTalentExcerpt;
