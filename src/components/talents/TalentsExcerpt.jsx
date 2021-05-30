f685b9f796f867fd7d7e42bead817e5bda5f185e
jesstoh <jesstoh23@gmail.com>
2021-05-30 10:55:46 +0800

    Create TalentsExcerpt to display singel talent as small card

---
+++ src/components/talents/TalentsExcerpt.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import { Badge, Col, Row, Card, Image } from 'react-bootstrap';
+
+function TalentExcerpt({ talent }) {
+  return (
+    <Card className="talent-card shadow-sm  bg-white rounded">
+      <a href={`/talents/${talent.user.id}`}>
+        <Card.Body>
+          <Row>
+            <Col sm="3" className='text-center'>
+              <div className="sqr-image-container rounded-circle d-inline-block" >
+                <Image className='img-fluid'
+                  src={talent.image}
+                //   className="img-fluid"
+                  alt={talent.username}
+                />
+              </div>
+            </Col>
+            <Col sm='8'>
+              <Card.Title>
+                {talent.user.first_name}, {talent.user.last_name}{' '}
+              </Card.Title>
+              <Card.Subtitle className="mb-2 text-muted">
+                Review: ..... <br />
+                Gigs Won: {talent.gigs_won}
+              </Card.Subtitle>
+
+              <div>
+                {talent.skills.map((skill, index) => {
+                  return (
+                    <Badge pill variant="info" key={index} className="mr-2">
+                      {skill.name}
+                    </Badge>
+                  );
+                })}
+              </div>
+              <Card.Text className="text-truncate">{talent.bio}</Card.Text>
+            </Col>
+          </Row>
+        </Card.Body>
+      </a>
+    </Card>
+  );
+}
+
+export default TalentExcerpt;
