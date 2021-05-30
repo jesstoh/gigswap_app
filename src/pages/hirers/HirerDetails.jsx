3952ad95fad1a69b643a5a58f9e7d97c53859170
jesstoh <jesstoh23@gmail.com>
2021-05-30 17:02:54 +0800

    Fetch hirerDetails upon first rendering of HirerDetail page component

--- src/pages/hirers/HirerDetails.jsx
+++ src/pages/hirers/HirerDetails.jsx
@@ -1,11 +1,44 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { Spinner, Alert, Container } from 'react-bootstrap';
+import { fetchSingleHirer } from '../../slices/hirersSlice';
 
-function HirerDetails() {
-  return (
-    <div>
-      <h1>Hirer Show</h1>
-    </div>
+function HirerDetails({ match }) {
+  const dispatch = useDispatch();
+  //States of active hirer being viewed
+  const { hirer, status, error } = useSelector(
+    (state) => state.hirers.activeHirer
   );
+
+  //Hirer id in url
+  const { hirerId } = match.params;
+
+  useEffect(() => {
+    //Fetch single hirer details to the store
+    dispatch(fetchSingleHirer(hirerId));
+  }, []);
+
+  let content;
+
+  // If status is loading, show spinner
+  if (status === 'loading') {
+    content = (
+      <div className="text-center mt-5">
+        <Spinner animation="border" variant="primary" />
+      </div>
+    );
+  } else if (status === 'succeeded') {
+    content = <div>{hirer.username}</div>;
+  } else if (status === 'failed') {
+    //Show error if fetch failed
+    content = (
+      <Alert variant="danger" className="text-center">
+        {error}
+      </Alert>
+    );
+  }
+
+  return <React.Fragment>{content}</React.Fragment>;
 }
 
 export default HirerDetails;
