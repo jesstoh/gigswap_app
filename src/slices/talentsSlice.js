bd0aaa54d7c82107f0e8693e3a8c94075ee60ffc
jesstoh <jesstoh23@gmail.com>
2021-05-30 11:12:26 +0800

    Create fetchSingleTalent thunk

--- src/slices/talentsSlice.js
+++ src/slices/talentsSlice.js
@@ -23,12 +23,28 @@ export const fetchTalents = createAsyncThunk(
   }
 );
 
+//Fetching particular talent profile by talent id
+export const fetchSingleTalent = createAsyncThunk(
+  'talents/fetchSingleTalent',
+  async (talentId, { rejectWithValue }) => {
+    try {
+      const response = await Axios.get(
+        `${process.env.REACT_APP_API_URL}/api/talents/${talentId}/`
+      );
+      return response.data;
+    } catch (err) {
+      const { data, status } = err.response;
+      return rejectWithValue({ data, status });
+    }
+  }
+);
+
 const talentsSlice = createSlice({
   name: 'talents',
   initialState,
   reducers: {},
   extraReducers: (builder) => {
-    // Update talent favourite list when fetch for login talent success
+    // Update talent lists when fetch at login hirer landing page
     builder.addCase(fetchTalents.fulfilled, (state, action) => {
       state.talents = action.payload;
       state.status = 'succeeded';
@@ -41,6 +57,20 @@ const talentsSlice = createSlice({
     builder.addCase(fetchTalents.rejected, (state, action) => {
       state.status = 'failed';
       state.error = action.payload.data.detail;
+    });
+    // Update talent lists when fetch at login hirer landing page
+    builder.addCase(fetchSingleTalent.fulfilled, (state, action) => {
+      state.activeTalent.talent = action.payload;
+      state.activeTalent.status = 'succeeded';
+    });
+    // Change status to loading when fetching
+    builder.addCase(fetchSingleTalent.pending, (state, action) => {
+      state.activeTalent.status = 'loading';
+    });
+    // Change status to failed & set error, when not found
+    builder.addCase(fetchSingleTalent.rejected, (state, action) => {
+      state.activeTalent.status = 'failed';
+      state.activeTalent.error = action.payload.data.detail;
     });
   },
 });
