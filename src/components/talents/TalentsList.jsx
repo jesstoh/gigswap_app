d50e23f92886111427824133011d7a395d06814f
jesstoh <jesstoh23@gmail.com>
2021-05-30 10:56:01 +0800

    Able to display all talents with profile in container

---
+++ src/components/talents/TalentsList.jsx
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+import { useSelector } from 'react-redux';
+import { Container, Spinner, Row } from 'react-bootstrap';
+import TalentExcerpt from './TalentsExcerpt';
+
+function TalentsList() {
+  const { talents, status, error } = useSelector((state) => state.talents);
+  let content;
+
+  // If status is loading, show spinner
+  if (status === 'loading') {
+    content = (
+      <div className="text-center mt-5">
+        <Spinner animation="border" variant="primary" />
+      </div>
+    );
+  } else if (status === 'succeeded') {
+    if (!talents.length) {
+      //Show message if there is empty talent list, though unlikely
+      content = <span>No relevant talents</span>;
+    } else {
+      content = (
+        <Container>
+          {talents.map((talent) => (
+            <TalentExcerpt key={talent.id} talent={talent} />
+          ))}
+        </Container>
+      );
+    }
+  } else if (status === 'failed') {
+    content = <span>{error}</span>;
+  }
+
+  return <Container className="mt-5">{content}</Container>;
+}
+export default TalentsList;
