204240e9f596dbfdd950faefd1b5e723b07924a0
jesstoh <jesstoh23@gmail.com>
2021-05-30 16:55:47 +0800

    Create fetchSingleHirer thunk to fetch hirer detail

--- src/slices/hirersSlice.js
+++ src/slices/hirersSlice.js
@@ -8,11 +8,42 @@ const initialState = {
   activeHirer: { hirer: null, status: 'idle', error: null },
 };
 
+//Fetching particular hirer profile by id
+export const fetchSingleHirer = createAsyncThunk(
+  'hirers/fetchSingleHirer',
+  async (hirerId, { rejectWithValue }) => {
+    try {
+      const response = await Axios.get(
+        `${process.env.REACT_APP_API_URL}/api/hirers/${hirerId}/`
+      );
+      return response.data;
+    } catch (err) {
+      const { data, status } = err.response;
+      return rejectWithValue({ data, status });
+    }
+  }
+);
+
 const hirersSlice = createSlice({
   name: 'hirers',
   initialState,
   reducers: {},
-  extraReducers: (builder) => {},
+  extraReducers: (builder) => {
+    // Update state when successfully fetching hirer detail
+    builder.addCase(fetchSingleHirer.fulfilled, (state, action) => {
+      state.activeHirer.hirer = action.payload;
+      state.activeHirer.status = 'succeeded';
+    });
+    // Change status to loading when fetching
+    builder.addCase(fetchSingleHirer.pending, (state, action) => {
+      state.activeHirer.status = 'loading';
+    });
+    // Change status to failed & set error, when not found
+    builder.addCase(fetchSingleHirer.rejected, (state, action) => {
+      state.activeHirer.status = 'failed';
+      state.activeHirer.error = action.payload.data.detail;
+    });
+  },
 });
 
 export default hirersSlice.reducer;
