4394bfbbdd27edbca7605ebfc417dfc986351b90
jesstoh <jesstoh23@gmail.com>
2021-05-31 09:45:25 +0800

    Add condition to complete profile before posting gig

--- src/pages/gigs/AddGigForm.jsx
+++ src/pages/gigs/AddGigForm.jsx
@@ -2,20 +2,26 @@ import React, { useState, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { unwrapResult } from '@reduxjs/toolkit';
 import { Form, Button, Col, Alert, Container } from 'react-bootstrap';
-import {useHistory} from "react-router-dom"
+import { useHistory } from 'react-router-dom';
 import { format } from 'date-fns';
 import { addGig } from '../../slices/gigsSlice.js';
 
 function AddGigForm() {
   const dispatch = useDispatch();
-  const history = useHistory()
+  const history = useHistory();
   // Get all subcategories as options
   const subcategories = useSelector(
     (state) => state.categories.subcats.content
   );
   const status = useSelector((state) => state.gigs.status);
   const error = useSelector((state) => state.gigs.error);
-  const createdGig = useSelector(state => state.gigs.activeGig.gig)
+  const createdGig = useSelector((state) => state.gigs.activeGig.gig);
+
+  //Get state if hirer profile is completed
+  const isProfileComplete = useSelector(
+    (state) => state.authentication.isProfileComplete
+  );
+  const userStatus = useSelector((state) => state.authentication.status);
 
   const [errorMessage, setErrorMessage] = useState(null);
   const [formValue, setFormValue] = useState({
@@ -44,8 +50,8 @@ function AddGigForm() {
     if (!data.postal_code) {
       delete data.postal_code;
     }
-    data.expired_at = new Date(data.expired_at)
-    console.log(data)
+    data.expired_at = new Date(data.expired_at);
+    console.log(data);
     try {
       const result = await dispatch(addGig(data));
       unwrapResult(result);
@@ -76,11 +82,204 @@ function AddGigForm() {
   }
 
   useEffect(() => {
-    if(status === 'succeeded' && createdGig) {
-      console.log(createdGig)
-      history.push(`/gigs/${createdGig.id}`)
+    if (status === 'succeeded' && createdGig) {
+      console.log(createdGig);
+      history.push(`/gigs/${createdGig.id}`);
     }
-  }, [status])
+    if (!isProfileComplete) {
+      setErrorMessage(
+        // Display error message if hirer's profile is incomplete
+        'Please complete your company profile before posting gig.'
+      );
+    }
+  }, [status]);
+
+  let content = null;
+
+  if (userStatus === 'succeeded') {
+    if (!isProfileComplete) {
+      content = (
+        <div className="text-center mt-3">
+          <a className="btn btn-light px-4" href="/profile/create">
+            Link to complete profile
+          </a>
+        </div>
+      );
+    } else {
+      //display form
+      content = (
+        <Form onSubmit={handleSubmit}>
+          <h4 className="text-center mb-4">Create New Gig</h4>
+          <h6>Gig Info*</h6>
+          <Form.Row>
+            <Form.Group as={Col}>
+              <Form.Label>Gig Title</Form.Label>
+              <Form.Control
+                required
+                type="text"
+                name="title"
+                value={formValue.title}
+                onChange={handleChange}
+              />
+            </Form.Group>
+            <Form.Group as={Col}>
+              <Form.Label>Expired Date</Form.Label>
+              <Form.Control
+                type="date"
+                name="expired_at"
+                value={formValue.expired_at}
+                required
+                min={min_date}
+                onChange={handleChange}
+              ></Form.Control>
+            </Form.Group>
+          </Form.Row>
+
+          <Form.Group>
+            <Form.Label>description</Form.Label>
+            <Form.Control
+              as="textarea"
+              rows={7}
+              required
+              style={{ resize: 'none' }}
+              name="description"
+              value={formValue.description}
+              onChange={handleChange}
+            />
+          </Form.Group>
+          <h6 className="mt-5">Gig Detail *</h6>
+          <Form.Group>
+            <Form.Label>Category</Form.Label>
+            <Form.Control
+              required
+              as="select"
+              multiple
+              name="subcategories"
+              value={formValue.subcategories}
+              onChange={multiSelectChange}
+            >
+              {subcategories.map((subcat) => (
+                <option key={subcat.id} value={subcat.id}>
+                  {subcat.name}
+                </option>
+              ))}
+            </Form.Control>
+          </Form.Group>
+          <Form.Row>
+            <Form.Group as={Col}>
+              <Form.Label>Remote </Form.Label>
+              <Form.Check
+                type="checkbox"
+                label="Yes"
+                name="is_remote"
+                value={formValue.is_remote}
+                onChange={checkBoxChange}
+              />
+            </Form.Group>
+            <Form.Group as={Col}>
+              <Form.Label>Project or Fixed Term </Form.Label>
+              <Form.Check
+                type="checkbox"
+                label="Fixed Term"
+                name="is_fixed"
+                value={formValue.is_fixed}
+                onChange={checkBoxChange}
+              />
+            </Form.Group>
+          </Form.Row>
+          <Form.Row>
+            <Form.Group as={Col}>
+              <Form.Label>Hourly Rate, $ </Form.Label>
+              <Form.Control
+                type="number"
+                min="0"
+                name="hour_rate"
+                value={formValue.hour_rate}
+                onChange={handleChange}
+              />
+            </Form.Group>
+            <Form.Group as={Col}>
+              <Form.Label>Budget </Form.Label>
+              <Form.Control
+                type="number"
+                min="1"
+                name="fixed_amount"
+                value={formValue.fixed_amount}
+                onChange={handleChange}
+              />
+            </Form.Group>
+          </Form.Row>
+          <Form.Row>
+            <Form.Group as={Col}>
+              <Form.Label>Duration </Form.Label>
+              <Form.Control
+                type="number"
+                required
+                min="1"
+                name="duration"
+                value={formValue.duration}
+                onChange={handleChange}
+              />
+            </Form.Group>
+            <Form.Group as={Col}>
+              <Form.Label>Unit</Form.Label>
+              <Form.Control
+                as="select"
+                name="duration_unit"
+                value={formValue.duration_unit}
+                required
+                onChange={handleChange}
+              >
+                {duration_unit_options.map((unit, index) => (
+                  <option key={index} value={unit}>
+                    {unit}
+                  </option>
+                ))}
+              </Form.Control>
+            </Form.Group>
+          </Form.Row>
+
+          <h6 className="mt-5">Gig Location</h6>
+          <Form.Group>
+            <Form.Label>Address</Form.Label>
+            <Form.Control
+              type="text"
+              name="address"
+              value={formValue.address}
+              onChange={handleChange}
+            />
+          </Form.Group>
+          <Form.Row>
+            <Form.Group as={Col}>
+              <Form.Label>Postal Code</Form.Label>
+              <Form.Control
+                type="number"
+                name="postal_code"
+                value={formValue.postal_code}
+                onChange={handleChange}
+              />
+            </Form.Group>
+            <Form.Group as={Col}>
+              <Form.Label>Country</Form.Label>
+              <Form.Control
+                type="text"
+                name="country"
+                value={formValue.country}
+                onChange={handleChange}
+              />
+            </Form.Group>
+          </Form.Row>
+          <Form.Text className="text-muted">* Required Field</Form.Text>
+
+          <div className="text-center mt-3">
+            <Button variant="primary" className="px-4" type="submit">
+              Submit
+            </Button>
+          </div>
+        </Form>
+      );
+    }
+  }
 
   return (
     <Container className="mt-4">
@@ -93,176 +292,7 @@ function AddGigForm() {
           {errorMessage}
         </Alert>
       )}
-
-      <Form onSubmit={handleSubmit}>
-        <h4 className="text-center mb-4">Create New Gig</h4>
-        <h6>Gig Info*</h6>
-        <Form.Row>
-          <Form.Group as={Col}>
-            <Form.Label>Gig Title</Form.Label>
-            <Form.Control
-              required
-              type="text"
-              name="title"
-              value={formValue.title}
-              onChange={handleChange}
-            />
-          </Form.Group>
-          <Form.Group as={Col}>
-            <Form.Label>Expired Date</Form.Label>
-            <Form.Control
-              type="date"
-              name="expired_at"
-              value={formValue.expired_at}
-              required
-              min={min_date}
-              onChange={handleChange}
-            ></Form.Control>
-          </Form.Group>
-        </Form.Row>
-
-        <Form.Group>
-          <Form.Label>description</Form.Label>
-          <Form.Control
-            as="textarea"
-            rows={7}
-            required
-            style={{ resize: 'none' }}
-            name="description"
-            value={formValue.description}
-            onChange={handleChange}
-          />
-        </Form.Group>
-        <h6 className="mt-5">Gig Detail *</h6>
-        <Form.Group>
-          <Form.Label>Category</Form.Label>
-          <Form.Control
-            required
-            as="select"
-            multiple
-            name="subcategories"
-            value={formValue.subcategories}
-            onChange={multiSelectChange}
-          >
-            {subcategories.map((subcat) => (
-              <option key={subcat.id} value={subcat.id}>
-                {subcat.name}
-              </option>
-            ))}
-          </Form.Control>
-        </Form.Group>
-        <Form.Row>
-          <Form.Group as={Col}>
-            <Form.Label>Remote </Form.Label>
-            <Form.Check
-              type="checkbox"
-              label="Yes"
-              name="is_remote"
-              value={formValue.is_remote}
-              onChange={checkBoxChange}
-            />
-          </Form.Group>
-          <Form.Group as={Col}>
-            <Form.Label>Project or Fixed Term </Form.Label>
-            <Form.Check
-              type="checkbox"
-              label="Fixed Term"
-              name="is_fixed"
-              value={formValue.is_fixed}
-              onChange={checkBoxChange}
-            />
-          </Form.Group>
-        </Form.Row>
-        <Form.Row>
-          <Form.Group as={Col}>
-            <Form.Label>Hourly Rate, $ </Form.Label>
-            <Form.Control
-              type="number"
-              min="1"
-              name="hour_rate"
-              value={formValue.hour_rate}
-              onChange={handleChange}
-            />
-          </Form.Group>
-          <Form.Group as={Col}>
-            <Form.Label>Budget </Form.Label>
-            <Form.Control
-              type="number"
-              min="1"
-              name="fixed_amount"
-              value={formValue.fixed_amount}
-              onChange={handleChange}
-            />
-          </Form.Group>
-        </Form.Row>
-        <Form.Row>
-          <Form.Group as={Col}>
-            <Form.Label>Duration </Form.Label>
-            <Form.Control
-              type="number"
-              required
-              min="1"
-              name="duration"
-              value={formValue.duration}
-              onChange={handleChange}
-            />
-          </Form.Group>
-          <Form.Group as={Col}>
-            <Form.Label>Unit</Form.Label>
-            <Form.Control
-              as="select"
-              name="duration_unit"
-              value={formValue.duration_unit}
-              required
-              onChange={handleChange}
-            >
-              {duration_unit_options.map((unit, index) => (
-                <option key={index} value={unit}>
-                  {unit}
-                </option>
-              ))}
-            </Form.Control>
-          </Form.Group>
-        </Form.Row>
-
-        <h6 className="mt-5">Gig Location</h6>
-        <Form.Group>
-          <Form.Label>Address</Form.Label>
-          <Form.Control
-            type="text"
-            name="address"
-            value={formValue.address}
-            onChange={handleChange}
-          />
-        </Form.Group>
-        <Form.Row>
-          <Form.Group as={Col}>
-            <Form.Label>Postal Code</Form.Label>
-            <Form.Control
-              type="number"
-              name="postal_code"
-              value={formValue.postal_code}
-              onChange={handleChange}
-            />
-          </Form.Group>
-          <Form.Group as={Col}>
-            <Form.Label>Country</Form.Label>
-            <Form.Control
-              type="text"
-              name="country"
-              value={formValue.country}
-              onChange={handleChange}
-            />
-          </Form.Group>
-        </Form.Row>
-        <Form.Text className="text-muted">* Required Field</Form.Text>
-
-        <div className="text-center mt-3">
-          <Button variant="primary" className="px-4" type="submit">
-            Submit
-          </Button>
-        </div>
-      </Form>
+      {content}
     </Container>
   );
 }
