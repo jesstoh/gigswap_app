1916af022dde87edad6b8092e3207a261e0d0f4c
jesstoh <jesstoh23@gmail.com>
2021-06-04 19:36:02 +0800

    Complete logic of fetching users in AdminUserList page

--- src/pages/admin/AdminUserList.jsx
+++ src/pages/admin/AdminUserList.jsx
@@ -1,11 +1,37 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { Container, Row, Col, Spinner, Alert } from 'react-bootstrap';
+import { fetchUsers} from '../../slices/adminsSlice';
 
 function AdminUserList() {
-  return (
-    <div>
-      <h1>Admin User Page</h1>
-    </div>
-  );
+  const dispatch = useDispatch();
+  const { users, status, error } = useSelector((state) => state.admins);
+
+  // Fetch users upon first rendering
+  useEffect(() => {
+    dispatch(fetchUsers());
+  }, []);
+
+  let content;
+
+  if (status === 'loading') {
+    content = (
+      <div className="text-center mt-5">
+        <Spinner animation="border" variant="primary" />
+      </div>
+    );
+  } else if (status === 'succeeded') {
+    content = <Container>User List</Container>
+  }else if (status === 'failed') {
+    //Show error if fetch failed
+    content = (
+      <Alert variant="danger" className="text-center">
+        {error}
+      </Alert>
+    );
+  }
+
+  return <>{content}</>;
 }
 
 export default AdminUserList;
