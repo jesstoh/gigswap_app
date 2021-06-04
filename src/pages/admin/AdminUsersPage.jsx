cdbcb020bbe3941039636d51fa9b0db17c9a0072
jesstoh <jesstoh23@gmail.com>
2021-06-04 20:32:05 +0800

    Can toggle between hirers & talents

--- src/pages/admin/AdminUsersPage.jsx
+++ src/pages/admin/AdminUsersPage.jsx
@@ -1,17 +1,24 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { Container, Row, Col, Spinner, Alert } from 'react-bootstrap';
+import { Container, Spinner, Alert, ToggleButton, ToggleButtonGroup } from 'react-bootstrap';
 import { fetchUsers } from '../../slices/adminsSlice';
 import UsersList from '../../components/admins/UsersList';
 
 function AdminUsersPage() {
   const dispatch = useDispatch();
   const { users, status, error } = useSelector((state) => state.admins);
+  // Set toggle button value
+  const [isHirer, setIsHirer] = useState(false);
 
   // Fetch users upon first rendering
   useEffect(() => {
     dispatch(fetchUsers());
   }, []);
+
+  // For toggling between talents & hirers
+  function handleToggleChange(val){
+    setIsHirer(val)
+  }
 
   let content;
 
@@ -23,8 +30,13 @@ function AdminUsersPage() {
     );
   } else if (status === 'succeeded') {
     content = (
-      <Container>
-        <UsersList users={users.talents} />
+      <Container className='px-5 py-3 my-3'>
+        <h1 className='text-center'>User List</h1>
+        <ToggleButtonGroup type='radio' name='isHirer' value={isHirer} onChange={handleToggleChange} >
+          <ToggleButton value={false} variant='outline-primary'>Talent</ToggleButton>
+          <ToggleButton value={true} variant='outline-primary'>Hirer</ToggleButton>
+        </ToggleButtonGroup>
+        {isHirer?<UsersList users={users.hirers}/> : <UsersList users={users.talents}/>}
       </Container>
     );
   } else if (status === 'failed') {
