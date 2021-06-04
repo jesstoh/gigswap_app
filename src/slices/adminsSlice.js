5bfb587ed156ae58b80e255f77d5d5bb43ccc5aa
jesstoh <jesstoh23@gmail.com>
2021-06-04 18:01:05 +0800

    Create fetchAminDashboard thunk to fetch summary of app

--- src/slices/adminsSlice.js
+++ src/slices/adminsSlice.js
@@ -3,17 +3,43 @@ import Axios from '../utilz/Axios';
 
 const initialState = {
     summary: null,
-    users: '',
+    users: [],
     status: 'idle',
     error: null,
 }
+
+// Fetch app summary
+export const fetchAdminDashboard = createAsyncThunk(
+    'admins/fetchAdminDashboard',
+    async (_, { rejectWithValue }) => {
+      try {
+        const response = await Axios.get(
+          `${process.env.REACT_APP_API_URL}/api/admins/dashboard/`
+        );
+        return response.data;
+      } catch (err) {
+        const { data, status } = err.response;
+        return rejectWithValue({ data, status });
+      }
+    }
+  );
 
 const adminsSlice = createSlice({
     name: 'admins',
     initialState,
     reducers: {},
     extraReducers: builder => {
-
+        builder.addCase(fetchAdminDashboard.fulfilled, (state, action) => {
+            state.summary = action.payload
+            state.status = 'succeeded';
+          });
+          builder.addCase(fetchAdminDashboard.pending, (state, action) => {
+            state.status = 'loading';
+          });
+          builder.addCase(fetchAdminDashboard.rejected, (state, action) => {
+            state.status = 'failed';
+            state.error = action.payload.data.detail;
+          });
     }
 })
 
