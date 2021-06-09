638b00829e487549dd17e3d07658daa3f56168ef
jesstoh <jesstoh23@gmail.com>
2021-06-10 01:34:51 +0800

    Update admin dashboard

--- src/pages/admin/AdminDashboard.jsx
+++ src/pages/admin/AdminDashboard.jsx
@@ -21,42 +21,73 @@ function AdminDashboard() {
     );
   } else if (status === 'succeeded') {
     content = (
-      <Container className="px-5 py-3 my-3">
-        <h2 className="text-center">Admin Dashboard</h2>
-        <Row className="text-center mt-3">
-          <Col className='p-3'>
-            <div className="border rounded full-size p-3">
+      <Container className="px-5 py-3 my-3" >
+        <h2 className="text-center mb-3">Admin Dashboard</h2>
+        <Row className="text-center mt-3 py-3 px-1 rounded shadow-sm" style={{'backgroundColor': 'rgba(173, 216, 230, 0.4)'}}>
+        <Col xs='12'className='text-left'><h4>USERS</h4></Col>
+          <Col xs='6' sm='3' className="p-2">
+            <div className="bg-white rounded full-size p-3">
+              <h5>ALL</h5>
+              <a href='/admin/maintenance/users' className='text-larger line-less'>{summary.usersCount}</a>
+            </div>
+          </Col>
+          <Col xs='6' sm='3' className="p-2">
+            <div className="bg-white rounded full-size p-3">
               <h5>TALENT</h5>
-              <span className='text-larger'> {summary.talentsCount}</span>
-             
+              <span className=" text-larger"> {summary.talentsCount}</span>
             </div>
           </Col>
-          <Col className='p-3'>
-            <div className="border rounded full-size p-3">
+          <Col xs='6' sm='3' className="p-2">
+            <div className="bg-white rounded full-size p-3">
               <h5>HIRER</h5>
-              <span className='text-larger'> {summary.hirersCount}</span>
+              <span className="text-larger"> {summary.hirersCount}</span>
             </div>
           </Col>
-          <Col className='p-3'>
-            <div className="border rounded full-size p-3">
-              <h5>ALL GIGS</h5>
-              <span className='text-larger'> {summary.gigsCount}</span>
-            </div>
-          </Col>
-          <Col className='p-3'>
-            <div className="border rounded full-size p-3">
-              <h5>ACTIVE GIGS</h5>
-              <span className='text-larger'> {summary.activeGigsCount}</span>
+          <Col xs='6' sm='3' className="p-2">
+            <div className="bg-white rounded full-size p-3">
+              <h6>PROFILE COMPLETE</h6>
+              <span className="text-larger">
+                {summary.profileCompleteRate}
+              </span>
             </div>
           </Col>
         </Row>
-        <Container className='mt-4 graph-container text-white text-center bg-dark border rounded p-3' style={{'minHeight': '50vh'}}>
+        <Row className="text-center mt-5 py-3 px-2 rounded shadow-sm" style={{'backgroundColor': 'rgba(211, 211, 211, 0.3)'}}>
+          <Col xs='12'className='text-left'><h4>GIGS</h4></Col>
+          <Col xs='6' sm='3' className="p-2">
+            <div className="bg-white rounded full-size p-3">
+              <h5>ALL</h5>
+              <a href='/admin/maintenance/gigs' className='text-larger line-less'>{summary.gigsCount}</a>
+            </div>
+          </Col>
+          <Col xs='6' sm='3' className="p-2">
+            <div className="bg-white rounded full-size p-3">
+              <h5>ACTIVE</h5>
+              <span className="text-larger"> {summary.activeGigsCount}</span>
+            </div>
+          </Col>
+          <Col xs='6' sm='3' className="p-2">
+            <div className="bg-white rounded full-size p-3">
+              <h5>AWARD</h5>
+              <span className="text-larger"> {summary.gigsAwardRate} %</span>
+            </div>
+          </Col>
+          <Col xs='6' sm='3' className="p-2">
+            <div className="bg-white rounded full-size p-3">
+              <h5>CANCEL</h5>
+              <span className="text-larger"> {summary.gigsCancelRate} %</span>
+            </div>
+          </Col>
+        </Row>
+        {/* <Container
+          className="mt-4 graph-container text-white text-center bg-dark border rounded p-3"
+          style={{ minHeight: '50vh' }}
+        >
           <h2>Placeholder for graph</h2>
-
-        </Container>
+        </Container> */}
       </Container>
     );
-  }else if (status === 'failed') {
+  } else if (status === 'failed') {
     //Show error if fetch failed
     content = (
       <Alert variant="danger" className="text-center">
